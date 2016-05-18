b19351eb01b575c13daeac0868466dc5e2abf48c
kknoerz <kevink430@gmail.com>
2016-05-17 20:43:13 -0400

    game.js

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -1,12 +1,21 @@
 $(document).ready(function(){
 	var queryUrl = '/game/api/' + $('#host').text();
-	console.log(queryUrl)
+	console.log('***************** This is queryURL: ', queryUrl)
 	$.ajax({
 	    dataType: 'json',
 	    url: queryUrl,
 	    method: 'GET'})
 	    .done(function(response) {
-	    	console.log(response.playerOne.img)
-	    	console.log(response.playerOne.username)
+	    	main(response.playerOne, response.playerTwo);
+				console.log('*************** here *********************');
 	    })
 });
+
+function main(playerOne, playerTwo) {
+	var p1 = this. playerOne;
+	var p2 = this.playerTwo;
+
+	console.log('This is playerOne: ', p1);
+
+	console.log('This is playerTwo: ', p2);
+}
