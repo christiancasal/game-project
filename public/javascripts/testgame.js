c52ef1127d75422e4374fcb8db15b78aa28dfd4a
Alexander <alexchasejones@icloud.com>
2016-05-23 16:37:00 -0400

    prevent scroll

--- public/javascripts/testgame.js
+++ public/javascripts/testgame.js
@@ -376,7 +376,12 @@ jQuery(document).ready(function () {
 	setTimeout( "jQuery('#loading_mask').fadeOut(600);", 1000 );
 });
 
-
+window.addEventListener("keydown", function(e) {
+    // space and arrow keys
+    if([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+        e.preventDefault();
+    }
+}, false);
 
 
 
