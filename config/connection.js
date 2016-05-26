092db3330054806d05bd0fcca0466c2c2cdc05b2
kknoerz <kevink430@gmail.com>
2016-05-26 11:49:06 -0400

    changes

--- config/connection.js
+++ config/connection.js
@@ -3,13 +3,14 @@
 // *********************************************************************************
 
 // Dependencies
-var Sequelize = require("sequelize"), connection;
+var Sequelize = require("sequelize"), sequelize;
 var flora = require('../arduino/flora.js');
 // console.log('this is connection.js');
 // console.log(flora);
 
 if(process.env.JAWSDB_URL) {
-  connection = new Sequelize(process.env.JAWSDB_URL);
+  var sequelize = new Sequelize(process.env.JAWSDB_URL);
+
 } else {
   // Lists out connection options
   var source = {
@@ -20,27 +21,26 @@ if(process.env.JAWSDB_URL) {
           database: "game_project"
       }
   }
+
+  // Selects a connection (can be changed quickly as needed)
+  var selectedSource = source.localhost;
+
+  // Creates mySQL connection using Sequelize
+
+  var sequelize = new Sequelize(selectedSource.database, selectedSource.user, selectedSource.password, {
+    define: { timestamps: false },
+    host: selectedSource.host,
+    dialect: 'mysql',
+
+    pool: {
+      max: 5,
+      min: 0,
+      idle: 10000
+    },
+
+  });
+
 }
-
-
-
-// Selects a connection (can be changed quickly as needed)
-var selectedSource = source.localhost;
-
-// Creates mySQL connection using Sequelize
-
-var sequelize = new Sequelize(selectedSource.database, selectedSource.user, selectedSource.password, {
-  define: { timestamps: false },
-  host: selectedSource.host,
-  dialect: 'mysql',
-
-  pool: {
-    max: 5,
-    min: 0,
-    idle: 10000
-  },
-
-});
 
 // Exports the connection for other files to use
 module.exports = sequelize;
