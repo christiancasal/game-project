f744296890b919ad5080974f3053b8fc001a8b10
kknoerz <kevink430@gmail.com>
2016-05-26 09:53:21 -0400

    added JAWSDB connection

--- config/connection.js
+++ config/connection.js
@@ -3,25 +3,32 @@
 // *********************************************************************************
 
 // Dependencies
-var Sequelize = require("sequelize");
+var Sequelize = require("sequelize"), connection;
 var flora = require('../arduino/flora.js');
 // console.log('this is connection.js');
 // console.log(flora);
 
-// Lists out connection options
-var source = {
-    localhost: {
-        host: 'localhost',
-        user: 'root',
-        password: "",
-        database: "game_project"
-    }
+if(process.env.JAWSDB_URL) {
+  connection = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  // Lists out connection options
+  var source = {
+      localhost: {
+          host: 'localhost',
+          user: 'root',
+          password: "",
+          database: "game_project"
+      }
+  }
 }
+
+
 
 // Selects a connection (can be changed quickly as needed)
 var selectedSource = source.localhost;
 
 // Creates mySQL connection using Sequelize
+
 var sequelize = new Sequelize(selectedSource.database, selectedSource.user, selectedSource.password, {
   define: { timestamps: false },
   host: selectedSource.host,
